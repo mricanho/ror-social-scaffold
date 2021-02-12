51fb416f2b67721fa48375557d05893f6fe6ea2a
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2021-02-12 22:22:16 +0530

    create friends method in the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,10 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  def friends
+    friends_array = friendships.map {|friendship| friendship.friend if friendship.status}
+    friends_array + inverse_friendships.map {|friendship| friendship.user if friendship.status}
+    friends_array.compact
+  end
 end
