7c9662e1d95e14847ee9e8276cb77bc43ee881d1
Miguel Ricaño <mricanho@gmail.com>
2021-02-12 15:48:15 -0600

    Add friendship helper

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,28 +11,4 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
-
-  def friends
-    friends_array = friendships.map { |friendship| friendship.friend if friendship.status }
-    friends_array += inverse_friendships.map { |friendship| friendship.user if friendship.status }
-    friends_array.compact
-  end
-
-  def pending_friends
-    friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
-  end
-
-  def friend_requests
-    inverse_friendships.map { |friendship| friendship.user unless friendship.status }.compact
-  end
-
-  def confirm_friend(user)
-    friendship_inverse = inverse_friendships.find { |friendship| friendship.user == user }
-    friendship_inverse.confirmed = true
-    friendship_inverse.save
-  end
-
-  def friend?(user)
-    friends.include? user
-  end
 end
